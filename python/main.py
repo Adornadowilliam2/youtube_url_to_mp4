076c9b384d38af1d131732f16ee590d2e1f14d95
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-01-18 13:45:52 +0800

    -add

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 from moviepy.editor import VideoFileClip
 
 # Load the video file
-video = VideoFileClip("invincible.mp4")
+video = VideoFileClip("sining.mp4")
 # need to install pip install moviepy
 # Extract audio and save as MP3
-video.audio.write_audiofile("invincible.mp3")
+video.audio.write_audiofile("sining.mp3")
